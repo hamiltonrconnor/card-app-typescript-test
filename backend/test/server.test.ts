86528ae7e0c767e50db5d71be31461ae585a0490
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2023-07-28 19:13:20 +0100

    Added dark mode and scheduled at

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,9 +1,8 @@
-import { server } from "../src/server"
 import Prisma from "../src/db";
+import { server } from "../src/server";
 
 describe("server test", () => {
   it("should assert 1 + 1 is 2", () => {
     expect(1 + 1).toEqual(2);
   });
 });
-
